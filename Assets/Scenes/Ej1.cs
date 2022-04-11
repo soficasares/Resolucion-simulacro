74c9b283c8687bb24fc14d57cbb32859274edd9f
soficasares <102155698+soficasares@users.noreply.github.com>
2022-04-11 20:08:06 -0300

    Ex 1 done it

---
+++ Assets/Scenes/Ej1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej1 : MonoBehaviour {
+    public int valor1;
+    public int valor2;
+
+    // 1.Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
+    void Start ()
+    {
+        if (valor1 == valor2)
+            Debug.Log("Son iguales");
+
+        else
+            Debug.Log("Son diferentes");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
